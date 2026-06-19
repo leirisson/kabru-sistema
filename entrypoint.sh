849816d2564a23f16c75aac4b804eaecf7767647
leirisson <103783610+leirisson@users.noreply.github.com>
2026-06-19 09:36:19 -0400

    rodar seed

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ echo "Rodando migrations..."
 node_modules/.bin/prisma migrate deploy
 
 echo "Rodando seed..."
-node_modules/.bin/prisma db seed
+node_modules/.bin/tsx prisma/seed.ts
 
 echo "Iniciando aplicação..."
 exec node server.js
