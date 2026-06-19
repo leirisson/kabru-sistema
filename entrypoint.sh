f021ad57f6b9d22124185d44ddd36021f04b8cea
leirisson <103783610+leirisson@users.noreply.github.com>
2026-06-19 09:21:15 -0400

    coorrigindo robustes

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,5 @@ set -e
 echo "Rodando migrations..."
 node_modules/.bin/prisma migrate deploy
 
-echo "Rodando seed..."
-node_modules/.bin/prisma db seed
-
 echo "Iniciando aplicação..."
 exec node server.js
