96efc5074b12c192710fd881edab4c79feed0cdd
leirisson <103783610+leirisson@users.noreply.github.com>
2026-06-19 09:35:05 -0400

    adicionando o seed

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,8 @@ set -e
 echo "Rodando migrations..."
 node_modules/.bin/prisma migrate deploy
 
+echo "Rodando seed..."
+node_modules/.bin/prisma db seed
+
 echo "Iniciando aplicação..."
 exec node server.js
