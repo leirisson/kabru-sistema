5d1592e620d62df26f77e8e341bcb8d594be1898
leirisson <103783610+leirisson@users.noreply.github.com>
2026-06-19 00:20:36 -0400

    subir a correção

--- system/next.config.ts
+++ system/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  serverExternalPackages: ['pdfjs-dist', 'pdf-parse'],
+  serverExternalPackages: ['pdf-parse'],
 };
 
 export default nextConfig;
